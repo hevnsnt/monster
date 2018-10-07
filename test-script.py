3f706bbc6f06e3a41f2a991e06a4bf2fcb245559
Bill Swearingen <hevnsnt@gmail.com>
2018-10-07 09:06:35 -0500

    First commit

---
+++ test-script.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#
+# _   .-')                     .-') _   .-')    .-') _     ('-.  _  .-')   
+#( '.( OO )_                  ( OO ) ) ( OO ). (  OO) )  _(  OO)( \( -O )  
+# ,--.   ,--.).-'),-----. ,--./ ,--,' (_)---\_)/     '._(,------.,------.  
+# |   `.'   |( OO'  .-.  '|   \ |  |\ /    _ | |'--...__)|  .---'|   /`. ' 
+# |         |/   |  | |  ||    \|  | )\  :` `. '--.  .--'|  |    |  /  | | 
+# |  |'.'|  |\_) |  |\|  ||  .     |/  '..`''.)   |  |  (|  '--. |  |_.' | 
+# |  |   |  |  \ |  | |  ||  |\    |  .-._)   \   |  |   |  .--' |  .  '.' 
+# |  |   |  |   `'  '-'  '|  | \   |  \       /   |  |   |  `---.|  |\  \  
+# `--'   `--'     `-----' `--'  `--'   `-----'    `--'   `------'`--' '--' 
+#                                                 IN A BOX          v1.0
+#
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+# init list with pin numbers
+
+pinList = [2, 3, 4, 17]
+
+# loop through pins and set mode and state to 'high'
+
+for i in pinList: 
+    GPIO.setup(i, GPIO.OUT) 
+    GPIO.output(i, GPIO.HIGH)
+
+# time to sleep between operations in the main loop
+
+SleepTimeL = 2
+
+# main loop
+
+try:
+  GPIO.output(2, GPIO.LOW)
+  print "ONE"
+  time.sleep(SleepTimeL); 
+  GPIO.output(3, GPIO.LOW)
+  print "TWO"
+  time.sleep(SleepTimeL);  
+  GPIO.output(4, GPIO.LOW)
+  print "THREE"
+  time.sleep(SleepTimeL);
+  GPIO.output(17, GPIO.LOW)
+  print "FOUR"
+  time.sleep(SleepTimeL);
+  GPIO.cleanup()
+  print "Good bye!"
+
+# End program cleanly with keyboard
+except KeyboardInterrupt:
+  print "  Quit"
+
+  # Reset GPIO settings
+  GPIO.cleanup()
+
+
+# find more information on this script at
+# https://github.com/hevnsnt/monster
+# Relay code from https://github.com/skiwithpete/relaypi
