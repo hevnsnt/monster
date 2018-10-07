affb0147d74985917be7050bc28639d791900ffc
Bill Swearingen <hevnsnt@gmail.com>
2018-10-07 14:06:28 -0500

    updated test script

--- test-script.py
+++ test-script.py
@@ -22,7 +22,7 @@ GPIO.setmode(GPIO.BCM)
 
 # init list with pin numbers
 
-pinList = [2, 3, 4, 17]
+pinList = [8, 10]
 
 # loop through pins and set mode and state to 'high'
 
@@ -37,20 +37,21 @@ SleepTimeL = 2
 # main loop
 
 try:
-  GPIO.output(2, GPIO.LOW)
+  GPIO.output(8, GPIO.LOW)
   print "ONE"
   time.sleep(SleepTimeL); 
-  GPIO.output(3, GPIO.LOW)
+  GPIO.output(10, GPIO.LOW)
   print "TWO"
   time.sleep(SleepTimeL);  
-  GPIO.output(4, GPIO.LOW)
-  print "THREE"
-  time.sleep(SleepTimeL);
-  GPIO.output(17, GPIO.LOW)
-  print "FOUR"
-  time.sleep(SleepTimeL);
-  GPIO.cleanup()
-  print "Good bye!"
+
+  #GPIO.output(4, GPIO.LOW)
+  #print "THREE"
+  #time.sleep(SleepTimeL);
+  #GPIO.output(17, GPIO.LOW)
+  #print "FOUR"
+  #time.sleep(SleepTimeL);
+  #GPIO.cleanup()
+  #print "Good bye!"
 
 # End program cleanly with keyboard
 except KeyboardInterrupt:
@@ -59,8 +60,8 @@ except KeyboardInterrupt:
   # Reset GPIO settings
   GPIO.cleanup()
 
-if __name__ == "__main__": # execute only if run as a script
-  domains, lnames, female, male = loadArray()
-  for x in range(int(argv[1])):
-    print getName()
+#if __name__ == "__main__": # execute only if run as a script
+#  domains, lnames, female, male = loadArray()
+#  for x in range(int(argv[1])):
+#    print getName()
 
