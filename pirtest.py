903225bb61c0911a4c63e76ecc777d11e18156d7
Bill Swearingen <hevnsnt@gmail.com>
2018-10-08 09:55:05 -0500

    pir test

--- pirtest.py
+++ pirtest.py
@@ -3,10 +3,10 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.IN)         #Read output from PIR motion sensor
+GPIO.setup(12, GPIO.IN)         #Read output from PIR motion sensor
 
 while True:
-	i=GPIO.input(18)
+	i=GPIO.input(12)
 	if i==0:                 #When output from motion sensor is LOW
 		print "No intruders",i
 	else:
