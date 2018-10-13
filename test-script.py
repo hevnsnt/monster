6b80b39b1d6c05c4d17ecd6a4ee5f1eab9cd4d2c
Bill Swearingen <hevnsnt@gmail.com>
2018-10-13 09:04:30 -0500

    updating test script

--- test-script.py
+++ test-script.py
@@ -55,8 +55,8 @@ def gpiosetup():
 
   # loop through pins and set mode and state to 'high'
   for i in pinList: 
-      GPIO.setup(i, GPIO.OUT) 
-      GPIO.output(i, GPIO.HIGH)
+    GPIO.setup(i, GPIO.OUT) 
+    GPIO.output(i, GPIO.HIGH)
   
 
 
