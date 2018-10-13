bcf9ea1fa39440789f22dbec78402c66fa7d3676
Bill Swearingen <hevnsnt@gmail.com>
2018-10-13 09:00:22 -0500

    updating test script

--- test-script.py
+++ test-script.py
@@ -142,4 +142,3 @@ if __name__ == "__main__": # execute only if run as a script
     print("    Find more information about this project at")
     print("    https://github.com/hevnsnt/monster")
     print("")
-
