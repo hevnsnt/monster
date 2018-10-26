56d20bedaecaff422a14d50dea30a5ca01853ef8
Bill Swearingen <hevnsnt@gmail.com>
2018-10-26 10:27:26 -0500

    Fixed stupid error

--- monster.py
+++ monster.py
@@ -102,8 +102,9 @@ if __name__ == "__main__": # execute only if run as a script
 	try:
 		os.system('cls' if os.name == 'nt' else 'clear')
 		nosmoke = False
-		if len(sys.argv) > 0 and sys.argv[1] == "-s":
-			nosmoke = True
+		if len(sys.argv) > 0: 
+				if sys.argv[1] == "-s":
+					nosmoke = True
 		os.system("cat monster.txt") # This is the easist way I know how to do this. CHANGE MY MIND
 		print("[+] Adjusting RaspberryPi Audio volume to 100%\n") #Set Raspi Audio Output all the way up
 		os.system("amixer sset PCM,0 200%") #Set Raspi Audio Output all the way up
